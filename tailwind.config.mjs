99dde5dca4bc7cde7341a2f12ac2c5ec134befe4
albertot-dev <atejero.dam@gmail.com>
2024-05-09 23:31:02 +0200

    First commit approach new design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import animations from "tailwindcss-animated";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        accent: "var(--color-accent)",
+      },
+      brightness: {
+        25: ".25",
+      },
+    },
+  },
+  plugins: [animations],
+};
