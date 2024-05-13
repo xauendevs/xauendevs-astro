20a9453b593a892813272420f58c103dc9a99e0b
albertot-dev <atejero.dam@gmail.com>
2024-05-13 21:33:07 +0200

    Added new section with partners

---
+++ src/consts/partners.ts
@@ -0,0 +1,14 @@
+import type { Partner } from "@/types/Partner";
+
+export const PARTNERS: Partner[] = [
+  {
+    name: "Cuatro Gatos Coworking",
+    url: "https://cuatrogatoscoworking.es/",
+    mainColor: "#00AC9C",
+  },
+  {
+    name: "Innovasur",
+    url: "https://www.innovasur.com/",
+    mainColor: "#ff4438",
+  },
+];
