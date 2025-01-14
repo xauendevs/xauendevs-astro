1cc9cebdcce9ae3333dad4f3551cc965c5a63c0b
albertot-dev <atejero.dam@gmail.com>
2025-01-14 13:05:14 +0100

    Date fixed and added animation details and design all pivos

--- src/lib/format-date.ts
+++ src/lib/format-date.ts
@@ -21,11 +21,9 @@ export function formatTime(date: Date): string {
 }
 
 export function formatDateTime(date: Date): string {
-  const day = date.getDate().toString().padStart(2, "0");
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const year = date.getFullYear();
-  const hour = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-
-  return `${day}/${month}/${year} * ${hour}:${minutes}`;
+  return format({
+    date,
+    format: "DD/MM/YYYY * HH:mm",
+    tz: "Europe/Madrid",
+  });
 }
